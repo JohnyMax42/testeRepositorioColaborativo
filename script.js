5a717186303f0be0c246279ea982a84076a46a26
JohnyMax42 <johnyernesto1997@gmail.com>
2024-06-21 16:56:22 -0300

    introdução a comandos js

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = 'Johny'
+
+let numero
+
+alert('Olá ' + nome)
+
+console.log(nome + numero)
+
+// entrada de texto
+
+let entradaUsuario = prompt("digite seu CPF")
+
+numero = confirm('Deseja fazer seu cartão da C&A')
